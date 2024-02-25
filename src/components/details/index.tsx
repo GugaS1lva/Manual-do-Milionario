63b6410574c2ef89870e4f3827ead69f30837924
Gustavo Silva <contatogustavosilva017@outlook.com>
2024-02-25 06:34:12 -0300

    Finished [✓]

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -4,16 +4,16 @@ import { Star } from "../../assets/Star"
 
 export function Details() {
   return (
-    <section className="flex flex-col sm:flex-row items-center justify-between">
-      <div className="w-[350px] sm:w-[550px] h-[240px] sm:h-[406px] flex flex-col justify-between items-center sm:items-start text-center sm:text-start">
-        <h2 className="font-semibold text-[32px] sm:text-[48px] leading-[130%] sm:leading-[160%]">Conquiste a riqueza que sempre desejou.</h2>
+    <section className="flex flex-col lg:flex-row items-center lg:justify-center xl:justify-between">
+      <div className="w-[350px] sm:w-[550px] md:my-[30px] h-[240px] sm:h-[406px] flex flex-col justify-between items-center lg:items-start text-center lg:text-start">
+        <h2 className="font-semibold text-[32px] sm:text-[48px] leading-[130%] md:leading-[160%]">Conquiste a riqueza que sempre desejou.</h2>
 
         <p className="text-[16px] sm:text-[18px] text-white/70 w-[320px] sm:w-[500px]">Este curso intensivo é composto por mais de 30 horas de conteúdo, dividido em módulos acessíveis e fáceis de acompanhar. Você terá a flexibilidade de estudar no seu próprio ritmo, adaptando-se à sua agenda.</p>
 
         <button className="hidden sm:block w-[170.3px] h-[64px] rounded-[10px] text-[18px] font-medium bg-[#FF9900] hover:bg-white text-black transition duration-75">Saiba Mais</button>
       </div>
 
-      <ul className="flex scale-75 sm:scale-100 flex-col gap-y-[36px]">
+      <ul className="flex scale-75 sm:scale-100 flex-col my-0 sm:my-[70px] gap-y-[36px]">
         <li className="flex items-center gap-[20px] hover:bg-gradient-to-b hover:from-white/30 hover:to-[#14101d] py-[16px] px-[20px] rounded-[20px] hover:cursor-pointer custom-hover-bg">
           <div className="w-[64px] h-[64px] rounded-[100%] bg-[#0E1013] flex flex-col items-center justify-center icon">
             <Star />
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,26 +3,26 @@ import { DiscountIcon } from "../../assets/DiscountIcon"
 
 export function Hero() {
   return (
-    <section className="pt-[42px] sm:pt-[80px]">
-      <h1 className="text-gradient text-[50px] sm:text-[91.73px] font-semibold text-center text-[#D99836]">Manual do Milionário</h1>
+    <section className="pt-[42px] sm:pt-[80px] flex flex-col items-center">
+      <h1 className="text-gradient text-[50px] sm:text-[74px] md:text-[91.73px] font-semibold text-center text-[#D99836]">Manual do Milionário</h1>
 
-      <div className="mt-[52px] sm:mt-[142px] flex sm:flex-row flex-col items-center justify-between">
-        <div className="flex flex-col items-center sm:items-start gap-[42px] text-center sm:text-start">
+      <div className="mt-[52px] md:mt-[92px] flex lg:flex-row flex-col items-center justify-center xl:gap-x-[190px]">
+        <div className="flex flex-col items-center lg:items-start gap-[42px] text-center lg:text-start">
           <span className="w-[343px] sm:w-[408px] py-2 pr-4 sm:pr-8 pl-[10px] text-white/60 bg-gradient-to-r from-[#272727] to-[#11101D] transform rounded-[10px] text-[14px] sm:text-[16px] flex items-center justify-between font">
             <DiscountIcon />
 
             <span className="text-white">20%</span> DESCONTO NO <span className="text-white">1 MÊS</span> DE ACESSO
           </span>
 
-          <h2 className="text-[24px] sm:text-[32.28px] mx-[32px] sm:mx-0 font-semibold max-w-[490px]">Destrave sua mente e torne-se o Primeiro Milionário da família.</h2>
+          <h2 className="text-[24px] sm:text-[32.28px] mx-[32px] sm:mx-0 font-semibold max-w-[375px]">Destrave sua mente e torne-se o Primeiro Milionário da família.</h2>
 
-          <p className="text-[#FFFFFF]/70 w-[320px] sm:w-[483px] leading-[170%] text-[18px] ">Descubra os segredos que os milionários usam para alcançar o sucesso financeiro neste curso abrangente e prático.</p>
+          <p className="text-[#FFFFFF]/70 w-[320px] lg:w-[483px] leading-[170%] text-[18px] -mt-[12px] sm:mt-0">Descubra os segredos que os milionários usam para alcançar o sucesso financeiro neste curso abrangente e prático.</p>
         </div>
 
-        <Image className="my-[60px] sm:my-0" src={"/HeroBanner.png"} width={508} height={354} alt="Hero Banner" />
+        <Image className="my-[70px] lg:my-[30px] px-[16px] sm:px-0 sm:mx-0 w-[428px] md:w-[468px] lg:w-[568px]" src={"/HeroBanner.png"} width={508} height={354} alt="Hero Banner" />
       </div>
 
-      <ul className="flex flex-col sm:flex-row items-center justify-between mt-[20px] mb-[60px] sm:my-[172px] gap-y-[12px] sm:gap-0">
+      <ul className="xl:scale-100 lg:scale-[82%] lg:w-[1184px] flex flex-col lg:flex-row items-center justify-between mt-[20px] mb-[60px] sm:my-[42px] lg:my-[172px] gap-y-[12px] sm:gap-0">
         <li className="flex items-center gap-[10px] sm:gap-[18px]">
           <strong className="font-semibold text-[31.64px] sm:text-[40.89px]">1350+</strong>
           <span className="text-[15.82px] sm:text-[20.45px] text-[#E6BF5C]">AULAS PRONTAS</span>
